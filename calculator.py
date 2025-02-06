b0570036b0e3204f77f9fbfaad1a6302e7abde1a
Anshul Roy <royanshul5002@gmail.com>
2025-02-06 11:16:04 +0530

    Upated calcualtor to add the subtract function

--- calculator.py
+++ calculator.py
@@ -1,9 +1,18 @@
 def add(x,y):
     return x+y
 
+def subtract(x,y):
+    return x-y
 print("enter two numbers")
 num1=int(input())
 num2=int(input())
 
-print(f"result,{add(num1,num2)}")
+choice=int(input("1 for additon enter 2 for subtraction")
+if choice==1:
+    print(add(num1,num2)
+elif choice==2:
+    print(subtract(num1,num2))
+
+
+
 
